c57522fd23c9cbbb20bd79b9e96c1a2812d21a03
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-02-14 16:26:23 +0530

    Predicting Name,Age and Count from  API
    Based on the input for name,we can predict the age using this API :
    
    https://api.agify.io/?name=${name}
    
    Made use of Axios Library to fetch the API,and useState to predict the age.
    
    We set the initial state to be null,then display all the values of the objects after getting the response,instead of just displaying the age.

--- src/App.js
+++ src/App.js
@@ -11,19 +11,21 @@ import Axios from "axios";
 
 function App() {
   const [name,setName]=useState(" ");
-  const [predictedAge,setPredictedAge]=useState(0);
+  const [predictedAge,setPredictedAge]=useState(null);
 
   const fetchData=()=>{
     Axios.get(`https://api.agify.io/?name=${name}`)
     .then((response)=>{
-      setPredictedAge(response.data.age);
+      setPredictedAge(response.data);
     });//Best Practice to use backticks,to declare a variable inside a string
   }
   return (
     <div className="App">
       <input placeholder="Ex. Akshay..." onChange={(event)=>{setName(event.target.value);}}/>
       <button onClick={fetchData}>Predict Age</button>
-      <h1>Predicted Age : {predictedAge}</h1>
+      <h1>Name: {predictedAge?.name}</h1>
+      <h1>Age: {predictedAge?.age}</h1>
+      <h1>Count : {predictedAge?.count}</h1>
     </div>
   );
 }
