888e9033f3d816af8c9ffbc0d5f9b34cc8088061
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-02-14 15:43:23 +0530

    Generating Random Cat Facts from API
    Fetched API using Axios Library,also made use of the useEffect Hook,To generate a single cat fact,while clicking the button.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import Axios from "axios";
+
+//In  JavaScript-->This is they way to fetch data from an API
+// fetch("https://catfact.ninja/fact")
+// .then((response) => response.json())//converts the response to json
+// .then((data) => {
+//   console.log(data);-->data is the json object
+//});
 
 function App() {
+  const [catFact, setCatfact] = useState("");
+   
+  const fetchCatFact = () => {
+    Axios.get("https://catfact.ninja/fact").then((res) => {
+      setCatfact(res.data.fact);
+    });
+  };
+  useEffect(() => {
+    fetchCatFact();
+  }, []);
+
   return (
     <div className="App">
-     <button>Generate Cat Fact</button>
+      <button onClick={fetchCatFact}>Generate Cat Fact</button>
+      <p>{catFact}</p>
     </div>
   );
 }
